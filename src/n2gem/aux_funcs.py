9493034a3787f024e332ed599bf09b6505092d04
venkat31 <d.venkatesh@hzdr.de>
2021-06-01 10:05:59 +0200

    modularized code with build_index & index_add funcs

--- src/n2gem/aux_funcs.py
+++ src/n2gem/aux_funcs.py
@@ -10,14 +10,76 @@ import time
 DEVICE_STRING = 'cuda' if torch.cuda.is_available() else 'cpu'
 GLOBAL_DEVICE = torch.device(DEVICE_STRING)
 
+def build_index(index, ngpus):
+    """
+    function to build an index on the gpu
+    
+    Parameters
+    ------------------
+    index : TYPE - faiss index of the type indexxflatl2 or indxivflat
+    
+    ngpus : TYPE - integer, indicating the number of gpus
+    
+    Return
+    ---------------
+    ind_tree : TYPE - faiss index built using gpu
+    """
+    gpu_resource = faiss.StandardGpuResources() # declare a gpu memory
+    if not ngpus > 1:
+        ind_tree = faiss.index_cpu_to_gpu(gpu_resource, 0, index)
+    else:
+        ind_tree = faiss.index_cpu_to_all_gpus(index)
+    
+    return ind_tree
 
+
+def index_add(index, dataset, n_samples, index_type, n_cels=100):
+    """
+    function to add dataset to the tree
+    
+    Parameters
+    --------------
+    index : TYPE - faiss index of the type indexxflatl2 or indxivflat
+    
+    dataset : TYPE - TYPE - torch tensor of N real samples of dimensionality D,
+                    DESCRIPTION - shape should be (N,D)
+    
+    nsamples : TYPE - integer
+               DESCRIPTION - number of samples to be considered for building the tree
+    
+    index_type : TYPE - string
+                 DESCRIPTION - the index type to choose from faiss ['indexflatl2', 'indexivfflat']
+                                default - 'indexflatl2'
+    
+    n_cells : TYPE - integer
+              DESCRIPTION - number of voronoi cells
+                              default = 100
+    
+   Return
+   ---------------
+   index - faiss tree of given index_type with the dataset added to the tree
+   
+    """
+    
+    s_time = time.time()
+    index.add(dataset[:n_samples, :])
+    e_time = time.time() - s_time
+    
+    if not index_type == 'indexflatl2':
+        print(f"Creating the tree by {index_type} using {n_cells} cells took {e_time:.5f} sec.")
+    else:
+        print(f"Creating the tree by {index_type} took {e_time:.5f} sec")
+    
+    return index
+
+                
 def build_tree_gem(train_samples, nsamples, index_type='indexflatl2', n_cells=100, seed=42):
     """
     Build a faiss tree
     
     Parameters
     -----------------
-    train_samples : TYPE - numpy array of N real samples of dimensionality D,
+    train_samples : TYPE - torch tensor of N real samples of dimensionality D,
                     DESCRIPTION - shape should be (N,D)
                     
     nsamples : TYPE - integer
@@ -53,66 +115,41 @@ def build_tree_gem(train_samples, nsamples, index_type='indexflatl2', n_cells=10
     
     # build the tree
     index = faiss.IndexFlatL2(dataset.shape[-1])
-    gpu_resource = faiss.StandardGpuResources() # declare a gpu memory
+    #gpu_resource = faiss.StandardGpuResources() # declare a gpu memory
     
     
     if ('cuda' in DEVICE_STRING):
+        
         # indexflatl2 in gpu
         if (index_type == 'indexflatl2'):
-            if not ngpus > 1:
-                index_tree = faiss.index_cpu_to_gpu(gpu_resource, 0, index)
-            else:
-                index_tree = faiss.index_cpu_to_all_gpus(index)
-                
-
-            start_time = time.time()
-            index_tree.add(dataset[:total_samples, :])
-            delta_time = time.time() - start_time
-            print(f"Creating the tree by {index_type} took {delta_time:.5f} sec")
-
-            return index_tree
+            index_tree = build_index(index, ngpus)
+            return index_add(index_tree, dataset, total_samples, index_type)
             
         # indexivfflat in gpu    
         elif (index_type == 'indexivfflat'):
             flat_index = faiss.IndexIVFFlat(index, dataset.shape[-1], n_cells)
-            if not ngpus > 1:
-                flat_ind_gpu = faiss.index_cpu_to_gpu(gpu_resource, 0, flat_index)
-            else:
-                flat_ind_gpu = faiss.index_cpu_to_all_gpus(flat_index)
-            
+            flat_ind_gpu = build_index(flat_index, ngpus)
             flat_ind_gpu.train(dataset)
-            start_time = time.time()
-            flat_ind_gpu.add(dataset[:total_samples, :])
-            delta_time = time.time() - start_time
-            print(f"Creating the tree by {index_type} using {n_cells} cells took {delta_time:.5f} sec.") 
-
-            return flat_ind_gpu
+            return index_add(flat_ind_gpu, dataset, total_samples, index_type, n_cells)
             
         else:
             print(f"Other index type not available")
       
     
     elif ('cpu' in DEVICE_STRING):
-        if (index_type == 'indexflatl2'):
-            start_time = time.time()
-            index.add(dataset[:total_samples, :])
-            delta_time = time.time() - start_time
-            print(f"Creating the tree by {index_type} took {delta_time:.5f} sec")
-            return index
         
+        # indexflatl2 in cpu
+        if (index_type == 'indexflatl2'):
+            index_flat_cpu_tree = index_add(index, dataset, total_samples, index_type)
+            return index_add(index, dataset, total_samples, index_type)
+        
+        # indexivflat in cpu
         elif (index_type == 'indexivfflat'):
             flat_index = faiss.IndexIVFFlat(index, dataset.shape[-1], n_cells)
             flat_index.train(dataset)
-
-            start_time = time.time()
-            flat_index.add(dataset[:total_samples, :])
-            delta_time = time.time() - start_time
-            print(f"Creating the tree by {index_type} using {n_cells} cells took {delta_time:.5f} sec.") 
-
-            return flat_index
+            return index_add(flat_index, dataset, total_samples, index_type, n_cells)
+        
         else:
             print(f"Other index type not available")
 
-    else:
-        print(f"Other index types not available")
         
